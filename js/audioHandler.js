8cf0756310513a8a9f6d678f915d446d8cb1029e
John Brilla <jbrilla17@gmail.com>
2016-07-01 22:05:39 +1000

    grabbing from soundcloud and making list

--- js/audioHandler.js
+++ js/audioHandler.js
@@ -1,13 +1,42 @@
-function call_it(url) {
-	//https://api.soundcloud.com/users/jbcool17/tracks?client_id=CLIENT_ID
-	$.ajax({ url: url,
-			success: function(result){
-				console.log("Calling it");
-				console.log(result);
-			}});
+var user = "jbcool17",
+	songList = {},
+	url = "https://api.soundcloud.com/users/" + user + "/tracks?client_id=" + CLIENT_ID,
+	scData = $.ajax({ url: url,
+				 	  success: function(result){
+						console.log(result);
+						createView();
+					}});
+
+
+
+function setupAudio (stream_link) {
+		var context = new AudioContext(),
+		audio = new Audio(),
+		source, 
+		url = stream_link + "?client_id=" + CLIENT_ID;
+		// audio.autoplay = true;
+		audio.src = url;
+		audio.crossOrigin = "anonymous";
+		source = context.createMediaElementSource(audio);
+		source.connect(context.destination);
+		source.mediaElement.play();
 }
 
+function createView() {
+	for (var i = 0; i < scData.responseJSON.length; i++){
+		songList[scData.responseJSON[i].title] = scData.responseJSON[i].stream_url;
+		$('#list').append("<li>Name: " + 
+			"<a href=" + scData.responseJSON[i].permalink_url + ">" + scData.responseJSON[i].title + 
+			"</a> Stream: " + 
+			scData.responseJSON[i].stream_url + 
+			" | <button id=" + scData.responseJSON[i].id + "> Play </button>" +
+			"</li>");
+		
+	}
+	console.log(songList);
+}
 
+// setupAudio(songList.Test01)
 // SC.initialize({
 //   client_id: 'CLIENT_ID'
 // });
@@ -20,11 +49,12 @@ function call_it(url) {
 // });
 // USERS - https://api.soundcloud.com/users/jbcool17?client_id=CLIENT_ID
 
-var a = $.ajax({url: "https://api.soundcloud.com/tracks/96883354.json?client_id=CLIENT_ID",
-				success: function(result){
-					console.log(result.stream_url)
-				}
-			})
+// var a = $.ajax({
+// 				url: "https://api.soundcloud.com/tracks/96883354.json?client_id=CLIENT_ID",
+// 				success: function(result){
+// 					console.log(result.stream_url)
+// 				}
+// 			})
 // var b = a.responseJSON.stream_url
 
 
@@ -32,15 +62,15 @@ var a = $.ajax({url: "https://api.soundcloud.com/tracks/96883354.json?client_id=
 // AUDIO API
 // #######################################################
 
-var context = new AudioContext(),
-audio = new Audio(),
-source,
-// `stream_url` you'd get from 
-// requesting http://api.soundcloud.com/tracks/6981096.json
-url = 'https://api.soundcloud.com/tracks/96883354/stream?client_id=CLIENT_ID';
-// audio.autoplay = true;
-audio.src = url;
-audio.crossOrigin = "anonymous";
-source = context.createMediaElementSource(audio);
-source.connect(context.destination);
-source.mediaElement.play();
+// var context = new AudioContext(),
+// audio = new Audio(),
+// source,
+// // `stream_url` you'd get from 
+// // requesting http://api.soundcloud.com/tracks/6981096.json
+// url = 'https://api.soundcloud.com/tracks/96883354/stream?client_id=CLIENT_ID';
+// // audio.autoplay = true;
+// audio.src = url;
+// audio.crossOrigin = "anonymous";
+// source = context.createMediaElementSource(audio);
+// source.connect(context.destination);
+// source.mediaElement.play();
