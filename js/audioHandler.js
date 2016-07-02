d3589aebf7d46aab238035bd519486aa617a3252
John Brilla <jbrilla17@gmail.com>
2016-07-02 12:14:24 +1000

    using sc api / starting

--- js/audioHandler.js
+++ js/audioHandler.js
@@ -1,76 +1,105 @@
-var user = "jbcool17",
-	songList = {},
-	url = "https://api.soundcloud.com/users/" + user + "/tracks?client_id=" + CLIENT_ID,
-	scData = $.ajax({ url: url,
-				 	  success: function(result){
-						console.log(result);
-						createView();
-					}});
 
 
+AudioHandler = {
+	user: "jbcool17",
+	url:  "https://api.soundcloud.com/users/jbcool17/tracks?client_id=" + CLIENT_ID, 
+	soundcloud: function() {
+		SC.initialize({
+	    	client_id: CLIENT_ID,
+	    	client_secret: CLIENT_SECRET
+	  	});
 
-function setupAudio (stream_link) {
-		var context = new AudioContext(),
-		audio = new Audio(),
-		source, 
-		url = stream_link + "?client_id=" + CLIENT_ID;
-		// audio.autoplay = true;
-		audio.src = url;
-		audio.crossOrigin = "anonymous";
-		source = context.createMediaElementSource(audio);
-		source.connect(context.destination);
-		source.mediaElement.play();
-}
-
-function createView() {
-	for (var i = 0; i < scData.responseJSON.length; i++){
-		songList[scData.responseJSON[i].title] = scData.responseJSON[i].stream_url;
-		$('#list').append("<li>Name: " + 
-			"<a href=" + scData.responseJSON[i].permalink_url + ">" + scData.responseJSON[i].title + 
-			"</a> Stream: " + 
-			scData.responseJSON[i].stream_url + 
-			" | <button id=" + scData.responseJSON[i].id + "> Play </button>" +
-			"</li>");
-		
+		var data = SC.get('/users/jbcool17/tracks').then(function(data) { console.log(data); });
+		return data;
 	}
-	console.log(songList);
 }
 
-// setupAudio(songList.Test01)
-// SC.initialize({
-//   client_id: 'CLIENT_ID'
-// });
+$(document).ready(function () {
+	
+	setTimeout(function (){ AudioHandler.soundcloud()}, 1000)	
+	
+})
 
-// var track_url = 'http://soundcloud.com/campgroundmusic/camp-ground-line-out';
-// SC.oEmbed(track_url, { auto_play: false }).then(function(oEmbed) {
-//   console.log('oEmbed response: ', oEmbed);
+//====================================================================================================================
+// AUDIO
+//====================================================================================================================
 
-//   $('#player').append(oEmbed.html)
-// });
-// USERS - https://api.soundcloud.com/users/jbcool17?client_id=CLIENT_ID
+		// Create a new instance of an audio object and adjust some of its properties
+		var audio = new Audio(), playing = false;
+		// audio.src = "<%= asset_path '052406.mp3' %>";
 
-// var a = $.ajax({
-// 				url: "https://api.soundcloud.com/tracks/96883354.json?client_id=CLIENT_ID",
-// 				success: function(result){
-// 					console.log(result.stream_url)
-// 				}
-// 			})
-// var b = a.responseJSON.stream_url
+		// https://soundcloud.com/headphoneactivist/silent-flo
+		SC.initialize({
+	    client_id: CLIENT_ID,
+	    client_secret: CLIENT_SECRET
+	  });
 
+		SC.get('/resolve', {
+			url: 'https://soundcloud.com/campgroundmusic/camp-ground-to-explode',
+			client_id: CLIENT_ID
+		}).then(function(track) {
+			audio.src = track.stream_url + '?client_id=' + CLIENT_ID;
+		});
 
-// #######################################################
-// AUDIO API
-// #######################################################
+		audio.controls = true;
+		audio.loop = true;
+		audio.autoplay = false;
+	  audio.crossOrigin = "anonymous";
 
-// var context = new AudioContext(),
-// audio = new Audio(),
-// source,
-// // `stream_url` you'd get from 
-// // requesting http://api.soundcloud.com/tracks/6981096.json
-// url = 'https://api.soundcloud.com/tracks/96883354/stream?client_id=CLIENT_ID';
-// // audio.autoplay = true;
-// audio.src = url;
-// audio.crossOrigin = "anonymous";
-// source = context.createMediaElementSource(audio);
-// source.connect(context.destination);
-// source.mediaElement.play();
+    $(audio).on('canplay', function() {
+    	if(!playing){
+	  		initMp3Player();
+	  	}
+		});
+
+		// Establish all variables that your Analyser will use
+		var canvas, ctx, source, context, analyser, fbc_array, bars, bar_x, bar_width, bar_height;
+
+		var frameLooper = function (){
+		    window.requestAnimationFrame(frameLooper);
+
+		    fbc_array = new Uint8Array(analyser.frequencyBinCount);
+		    analyser.getByteFrequencyData(fbc_array);
+
+		    for (var i = 0; i < scene.children.length; i++) {
+					var sceneChild = scene.children[i];
+					if (sceneChild.name === "Cube Particle" ) {
+
+						var scale = Math.pow((fbc_array[i] / 48), 3);
+						if ( scale > 0 ) {
+							sceneChild.scale.x = sceneChild.scale.y = sceneChild.scale.z = scale;
+						} else {
+							sceneChild.scale.x = sceneChild.scale.y = sceneChild.scale.z = 1;
+						}
+					}
+				}
+
+				var sum = 0;
+				for ( var i = 0; i < fbc_array.length; i++ ) {
+				    sum += parseInt( fbc_array[i], 10 ); //don't forget to add the base
+				}
+
+				var avg = sum / fbc_array.length;
+
+				if (avg) {
+					sceneRotationSpeed = 0.00065 * (avg / 10);
+				} else {
+					sceneRotationSpeed = 0.00015
+				}
+		}
+
+		var initMp3Player = function (){
+				playing = true;
+		    // document.getElementById('audio_box').appendChild(audio);
+		    context = new AudioContext(); // AudioContext object instance
+		    analyser = context.createAnalyser(); // AnalyserNode method
+		    canvas = document.getElementById('analyser_render');
+		    ctx = canvas.getContext('2d');
+		    // Re-route audio playback into the processing graph of the AudioContext
+		    source = context.createMediaElementSource(audio); 
+		    source.connect(analyser);
+		    analyser.connect(context.destination);
+		    // frameLooper();
+		}
+		// frameLooper() animates any style of graphics you wish to the audio frequency
+		// Looping at the default frame rate that the browser provides(approx. 60 FPS)
