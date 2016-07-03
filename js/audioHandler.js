80dc9540907183f38385301a002edbd6f7c1209a
John Brilla <jbrilla17@gmail.com>
2016-07-03 17:17:59 +1000

    styles for test viz

--- js/audioHandler.js
+++ js/audioHandler.js
@@ -44,10 +44,7 @@ AudioHandler = {
 		});
 	},
 	playStream: function(stream_url, audio){
-		if ($('audio').length === 1) {
-			// document.getElementById('audio_box').removeChild(audio);
-			audio.remove();
-		}
+		if ($('audio').length === 1) { audio.remove(); }
 		
 		SC.get('/tracks/' + stream_url).then(function(track) { 
 			audio.src = track.stream_url + '?client_id=' + CLIENT_ID;
@@ -66,35 +63,49 @@ AudioHandler = {
 		    fbc_array = new Uint8Array(analyser.frequencyBinCount);
 		    analyser.getByteFrequencyData(fbc_array);
 		    ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
-		    ctx.fillStyle = '#00CCFF'; // Color of the bars
+		    //'#00CCFF'; Color of the bars
+		    // ctx.fillStyle = '#00CCFF'
+		    //'#'+Math.floor(Math.random()*16777215).toString(16);
+
 		    bars = 100;
 		    for (var i = 0; i < bars; i++) {
 		        bar_x = i * 3;
 		        bar_width = 2;
 		        bar_height = -(fbc_array[i] / 2);
-		        // scene.children[i].scale.y = bar_height + 1;
-		        // scene.children[0].scale.x = bar_width * Math.random();
-		        //  fillRect( x, y, width, height ) // Explanation of the parameters below
 		        ctx.fillRect(bar_x, canvas.height, bar_width, bar_height);
+		        // console.log(bar_height)
+		        var bh = Math.abs(bar_height);
+		        
+		        if (bh > 90){
+					ctx.fillStyle = '#FF0000';
+		        } else if ( bh > 70) {
+		        	ctx.fillStyle = '#CC0000';
+		        } else if(bh > 50){
+		        	ctx.fillStyle = '#AAFFFF';
+		        }  else if(bh > 30){
+					ctx.fillStyle = '#55FFFF';
+		        }  else {
+		        	ctx.fillStyle = '#'+Math.floor(Math.random()*16777215).toString(16);
+		        }
+
+		        
 		    }
+		    
 		}
 
 		function initMp3Player(){
-			playing = true;
 		    document.getElementById('audio_box').appendChild(audio);
 		    context = new AudioContext(); // AudioContext object instance
 		    analyser = context.createAnalyser(); // AnalyserNode method
 		    canvas = document.getElementById('analyser_render');
 		    ctx = canvas.getContext('2d');
+
 		    // Re-route audio playback into the processing graph of the AudioContext
 		    source = context.createMediaElementSource(audio); 
 		    source.connect(analyser);
 		    analyser.connect(context.destination);
 		    frameLooper();
 		    $('#mp3_player').fadeIn(1000);
-		}
-		if (!playing) {
-			initMp3Player();
 		}
 
 		initMp3Player();
@@ -105,10 +116,10 @@ AudioHandler = {
 	},
 	initializeUiFunctions: function() {
 		$('button').on('click', function(e){
-			// console.log(e.toElement.id)
+			
 			var stream_url = "https://api.soundcloud.com/tracks/" + e.toElement.id + "/stream"
 			AudioHandler.playStream(e.toElement.id, audio)
-			// console.log(stream_url)
+			
 		})
 		console.log("(=>-Message: UI Initialized...")
 	}
