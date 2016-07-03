a3704244a6d03347128459e98ad728812a7b93e9
John Brilla <jbrilla17@gmail.com>
2016-07-03 12:01:00 +1000

    adding styles / hooking up player to viz / playing any track

--- js/audioHandler.js
+++ js/audioHandler.js
@@ -1,4 +1,6 @@
-
+var audio = new Audio(), playing = true;
+var sum = 0;
+var paused = false;
 
 AudioHandler = {
 	initialize: function () {
@@ -39,63 +41,44 @@ AudioHandler = {
 			AudioHandler.initializeUiFunctions();
 		});
 	},
-	playStream: function(stream_url){
-
-		var audio = new Audio(), playing = false;
+	playStream: function(stream_url, audio){
+		if ($('audio').length === 1) {
+			// document.getElementById('audio_box').removeChild(audio);
+			audio.remove();
+		}
+		
 		SC.get('/tracks/' + stream_url).then(function(track) { 
 			audio.src = track.stream_url + '?client_id=' + CLIENT_ID;
 			 
 		});
 			
-
 		audio.controls = true;
 		audio.loop = false;
 		audio.autoplay = true;
 	 	audio.crossOrigin = "anonymous";
 
-	    $(audio).on('canplay', function() {
-	    	if(!playing){
-		  		initMp3Player();
-		  	}
-		});
-
 		var canvas, ctx, source, context, analyser, fbc_array, bars, bar_x, bar_width, bar_height;
 
 		function frameLooper(){
 		    window.requestAnimationFrame(frameLooper);
-
 		    fbc_array = new Uint8Array(analyser.frequencyBinCount);
 		    analyser.getByteFrequencyData(fbc_array);
-
-		    for (var i = 0; i < scene.children.length; i++) {
-					var sceneChild = scene.children[i];
-					if (sceneChild.name === "Cube Particle" ) {
-
-						var scale = Math.pow((fbc_array[i] / 48), 3);
-						if ( scale > 0 ) {
-							sceneChild.scale.x = sceneChild.scale.y = sceneChild.scale.z = scale;
-						} else {
-							sceneChild.scale.x = sceneChild.scale.y = sceneChild.scale.z = 1;
-						}
-					}
-				}
-
-				var sum = 0;
-				for ( var i = 0; i < fbc_array.length; i++ ) {
-				    sum += parseInt( fbc_array[i], 10 ); //don't forget to add the base
-				}
-
-				var avg = sum / fbc_array.length;
-
-				if (avg) {
-					sceneRotationSpeed = 0.00065 * (avg / 10);
-				} else {
-					sceneRotationSpeed = 0.00015
-				}
+		    ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
+		    ctx.fillStyle = '#00CCFF'; // Color of the bars
+		    bars = 100;
+		    for (var i = 0; i < bars; i++) {
+		        bar_x = i * 3;
+		        bar_width = 2;
+		        bar_height = -(fbc_array[i] / 2);
+		        // scene.children[i].scale.y = bar_height + 1;
+		        // scene.children[0].scale.x = bar_width * Math.random();
+		        //  fillRect( x, y, width, height ) // Explanation of the parameters below
+		        ctx.fillRect(bar_x, canvas.height, bar_width, bar_height);
+		    }
 		}
 
 		function initMp3Player(){
-				playing = true;
+			playing = true;
 		    document.getElementById('audio_box').appendChild(audio);
 		    context = new AudioContext(); // AudioContext object instance
 		    analyser = context.createAnalyser(); // AnalyserNode method
@@ -105,8 +88,12 @@ AudioHandler = {
 		    source = context.createMediaElementSource(audio); 
 		    source.connect(analyser);
 		    analyser.connect(context.destination);
-		    // frameLooper();
+		    frameLooper();
 		}
+		if (!playing) {
+			initMp3Player();
+		}
+
 		initMp3Player();
 
 		console.log('STATUS CALL: playing stream...')
@@ -117,7 +104,7 @@ AudioHandler = {
 		$('button').on('click', function(e){
 			// console.log(e.toElement.id)
 			var stream_url = "https://api.soundcloud.com/tracks/" + e.toElement.id + "/stream"
-			AudioHandler.playStream(e.toElement.id)
+			AudioHandler.playStream(e.toElement.id, audio)
 			// console.log(stream_url)
 		})
 		console.log("STATUS CALL: UI Initialized...")
